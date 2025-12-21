38096b541f63dcf5c070753055fa2ec722dd2ae5
AnkitPandey123 <ankitpandey12304@gmail.com>
2025-12-21 16:28:10 +0530

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import trees.*;
+public class Main
+{
+    public static void main(String[] args)
+    {
+       int i = FindKthSmallestElementInBST.findKthSmallestElementInBST(2);
+        System.out.println(i);
+    }
+}
---
+++ src/trees/FindKthSmallestElementInBST.java
@@ -0,0 +1,8 @@
+package trees;
+
+public class FindKthSmallestElementInBST {
+    public static int findKthSmallestElementInBST(int B)
+    {
+        return 1;
+    }
+}
